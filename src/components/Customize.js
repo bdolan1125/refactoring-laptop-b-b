44551b5310c593e6c7679f20c442fd16cb5164bd
Benjamin Dolan <benjamin@thinkful.com>
2020-02-15 14:42:12 -0700

    final upload

---
+++ src/components/Customize.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Part from './Part';
+
+export default class Customize extends React.Component {
+
+  render() {
+    const features = Object.keys(this.props.features).map((feature, idx) => {
+      <Part updateFeature={this.props.updateFeature} features={this.props.features} feature={feature} idx={idx} USD={this.props.USD}/>
+})
+    return features;
+};
+}
--- src/cpu.js
+++ src/cpu.js
@@ -8,12 +8,14 @@ export default class Cpu extends Component {
       return(
       <div>
         <h3>Processors</h3>
-        <button onclick='selectItem()' key = '1'
+        <button onclick='selectItem()' 
+          key = '1'
           name = '17th Generation Intel Core HB'
           cost = '$700.00'>
           </button>
   
-        <button onclick='selectItem()' key = '2'
+        <button onclick='selectItem()' 
+          key = '2'
           name = 'Professor X AMD Fire Breather'
           cost = '$1,200.00'>
         </button>
